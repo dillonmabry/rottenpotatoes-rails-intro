8471d368670b7a55ad8f20f5e75d87e4f05238ab
Dillon Mabry <dmabry2@uncc.edu>
2015-10-29 03:10:58 +0000

    Part 3 testing part 3

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
-  # root :to => 'movies#index'
+  root :to => 'movies#index'
   
   resources :movies
   
